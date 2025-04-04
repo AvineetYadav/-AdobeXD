129361dce9e77f341027718ec83f900389dfe513
Avineet Yadav <12minute00@gmail.com>
2025-04-05 03:03:56 +0530

    add icon

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { Camera } from "lucide-react";
 
 const UserProfile = () => {
   const userDetails = useSelector((state) => state.user);
@@ -18,7 +19,9 @@ const UserProfile = () => {
               className="w-16 h-16 rounded-full"
             />
             <div className="absolute bottom-1 left-10">
-              {/* < className="w-5 h-5 text-gray-600 bg-white p-0.5 rounded-full" /> */}
+            <div className="absolute bottom-1 left-10">
+              <Camera className="w-5 h-5 text-gray-600 bg-white p-0.5 rounded-full" />
+            </div>
             </div>
           </div>
           <div className="ml-4">
